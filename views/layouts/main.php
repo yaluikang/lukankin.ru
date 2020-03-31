533619325cdb0acfc0ecff607d1b1ebbb859f0e0
yaluikang <lukankin.aleksandr@mail.ru>
2020-03-31 03:55:00 +0300

    Images v2.5

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -106,6 +106,4 @@ AppAsset::register($this);
 </footer>
 </body>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script type="application/javascript" src="index.js"></script>
-<script type="application/javascript" src="main-area/__content/__list-of-movies/main-area__content__list-of-movies.js"></script>
 </html>
