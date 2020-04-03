9b299a22f2cef3d999eacdb3fb2475805536e60c
yaluikang <lukankin.aleksandr@mail.ru>
2020-04-04 02:09:20 +0300

    Added test file js and added models/movies.php

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,9 +20,10 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/style.css',
+        'css/style.css'
     ];
     public $js = [
+        'js/index.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
