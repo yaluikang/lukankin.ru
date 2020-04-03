02bbce31a0911fd1a19e0e6eea5bd8af14108900
yaluikang <lukankin.aleksandr@mail.ru>
2020-04-04 02:19:36 +0300

    Testing js file 1.1

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -23,7 +23,7 @@ class AppAsset extends AssetBundle
         'css/style.css'
     ];
     public $js = [
-        'js/index.js'
+        //'js/index.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
