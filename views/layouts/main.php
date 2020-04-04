58cbc9d8779f44f9f2c38b84431320572b1e3d55
yaluikang <lukankin.aleksandr@mail.ru>
2020-04-04 11:37:02 +0300

    Added yii2 markup 1.1

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -18,8 +18,6 @@ $action = Yii::$app->controller->action->id;
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <?php $this->head(); ?>
-    <!--<link rel="stylesheet" href="css/style.css" type="text/css">
-    <link rel="stylesheet" href="css/newstyle.css" type="text/css">-->
 </head>
 <?php $this->beginBody(); ?>
 <body>
@@ -114,6 +112,5 @@ $action = Yii::$app->controller->action->id;
 <?php $this->EndBody(); ?>
 </body>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script type="application/javascript" src="js/index.js"></script>
 </html>
 <?php $this->endPage(); ?>
