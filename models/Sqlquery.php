6ba8a47fd66252b1b292a72ff642e82b4790ab41
yaluikang <lukankin.aleksandr@mail.ru>
2020-04-21 03:04:17 +0300

    Rebuilding Sqlquery 1.4

--- models/Sqlquery.php
+++ models/Sqlquery.php
@@ -13,18 +13,26 @@ class Sqlquery extends Query
         //$sql = $this->select(["movies_id, movies_name, movies_url_poster, movies_date, GROUP_CONCAT(genres_name SEPARATOR ', ') AS `genres_name` FROM (SELECT movies.movies_id, movies_name, genres_name, movies_url_poster, movies_date FROM movies JOIN movies_has_genre ON `movies`.`movies_id`=`movies_has_genre`.`movies_id` JOIN genres ON `movies_has_genre`.`movies_genre`=`genres`.`genres_id` WHERE `movies_qualifier`=1 AND movies.movies_id= ANY(SELECT movies.movies_id FROM movies JOIN movies_has_genre ON `movies`.`movies_id`=`movies_has_genre`.`movies_id` JOIN genres ON `movies_has_genre`.`movies_genre`=`genres`.`genres_id` WHERE `movies_qualifier`=1)) AS `table`"])->groupBy('movies_id')->limit(9)->all();
         //$sql = $this->createCommand();
         $borders = $this->getBorders();
-        $sql = $this->select("movies_id, movies_name, movies_url_poster, movies_date")->FROM("movies")->offset($borders['offset'])->limit($borders['limit'])->all();
-        return json_encode($sql,JSON_UNESCAPED_UNICODE);
+        $movies = $this->select("movies_id, movies_name, movies_url_poster, movies_date")->FROM("movies")->offset( $borders['offset'] )->limit( $borders['limit'] )->all();
+        $moviesWithGenres = $this->addGenresForMovies($movies);
+        return json_encode( $moviesWithGenres,JSON_UNESCAPED_UNICODE );
     }
     public function addGenresForMovies($movies)
     {
         //Вызвать функцию getIdOfMovies для составления запроса в базу данных по id
-        //
+        //цикл для инъекции жанров в массив $movies
+        $ids = $this->getIdOfMovies( $movies );
+        $genresOfMovies = $this->select('movies_id, genres_name' )->FROM('movies_has_genre' )->join('JOIN', 'genres', 'movies_has_genre.movies_genre=genres.genres_id' )->where(['movies_id' => $ids ])->all();
+        return array_merge_recursive( $movies, $genresOfMovies );
     }
-    public function getIdOfMovies()
+    public function getIdOfMovies($movies)
     {
         $arrayIdOfMovies = [];
-
+        for( $i = 0; $i < count($movies); $i++ )
+        {
+            array_push( $arrayIdOfMovies, $movies[0]['movies_id'] );
+        }
+        return $arrayIdOfMovies;
     }
     public function pagination()
     {
@@ -32,7 +40,7 @@ class Sqlquery extends Query
         //Задать новые лимит и оффсет для функции getMovies
         //Вызвать эту функцию с новыми лимитом и оффсетом
         $this->increasePageNumber();
-        $this->getMovies($this->getBorders());
+        $this->getMovies( $this->getBorders() );
     }
     public function getBorders()
     {
