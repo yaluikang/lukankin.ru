4a410fdf5ffa1b629a8857c8564ee52c9cf01444
yaluikang <lukankin.aleksandr@mail.ru>
2020-04-21 03:37:28 +0300

    Rebuilding Sqlquery 2.2

--- models/Sqlquery.php
+++ models/Sqlquery.php
@@ -7,7 +7,7 @@ use yii\db\Query;
 class Sqlquery extends Query
 {
     private $pageNumber = 1;
-    private $numOfMovies = 12;
+    private $numOfMovies = 9;
     public function getMovies()
     {
         //$sql = $this->select(["movies_id, movies_name, movies_url_poster, movies_date, GROUP_CONCAT(genres_name SEPARATOR ', ') AS `genres_name` FROM (SELECT movies.movies_id, movies_name, genres_name, movies_url_poster, movies_date FROM movies JOIN movies_has_genre ON `movies`.`movies_id`=`movies_has_genre`.`movies_id` JOIN genres ON `movies_has_genre`.`movies_genre`=`genres`.`genres_id` WHERE `movies_qualifier`=1 AND movies.movies_id= ANY(SELECT movies.movies_id FROM movies JOIN movies_has_genre ON `movies`.`movies_id`=`movies_has_genre`.`movies_id` JOIN genres ON `movies_has_genre`.`movies_genre`=`genres`.`genres_id` WHERE `movies_qualifier`=1)) AS `table`"])->groupBy('movies_id')->limit(9)->all();
@@ -22,7 +22,7 @@ class Sqlquery extends Query
         //Вызвать функцию getIdOfMovies для составления запроса в базу данных по id
         //цикл для инъекции жанров в массив $movies
         $ids = $this->getIdOfMovies( $movies );
-        $genresOfMovies = $this->select('movies_id, genres_name' )->FROM('movies_has_genre' )->join('JOIN', 'genres', 'movies_has_genre.movies_genre=genres.genres_id' )->where(['movies_id' => $ids ])->createCommand()->getRawSql();
+        $genresOfMovies = $this->select('movies_id, genres_name' )->FROM('movies_has_genre' )->join('JOIN', 'genres', 'movies_has_genre.movies_genre=genres.genres_id' )->where(['movies_id' => $ids ])->limit(0)->all();
         return $genresOfMovies; //array_merge_recursive( $movies, $genresOfMovies );
     }
     public function getIdOfMovies($movies)
