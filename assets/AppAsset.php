76751da20dcc644ab879ea3c026fa184811ea00e
yaluikang <lukankin.aleksandr@mail.ru>
2020-04-21 04:53:26 +0300

    Testing ajax addfilms 1.5

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -24,7 +24,7 @@ class AppAsset extends AssetBundle
         'css/newstyle.css'
     ];
     public $js = [
-        'js/index.js'
+        /*'js/index.js'*/
     ];
     public $depends = [
         'yii\web\YiiAsset'
