0885cf3e8daa42a4d315f3a388a29e5fc40e26e3
yaluikang <lukankin.aleksandr@mail.ru>
2020-04-25 01:30:21 +0300

    Added content to site/index.php

--- views/site/index.php
+++ views/site/index.php
@@ -1,1 +1,106 @@
-<h1>Welcome</h1>
+<div class="row MyMovies">
+    <ul class="nav nav-tabs col-md col-sm col-" id="myTab" role="tablist">
+        <li class="nav-item col-md-auto col-sm-auto col-auto paddingnol">
+            <a class="nav-link active films" id="home-tab" data-toggle="tab" href="#" role="tab" aria-controls="home" aria-selected="true">Фильмы</a>
+        </li>
+        <li class="nav-item col-md-auto col-sm-auto col-auto paddingnol">
+            <a class="nav-link tvSeries" id="profile-tab" data-toggle="tab" href="#" role="tab" aria-controls="profile" aria-selected="false">Сериалы</a>
+        </li>
+    </ul>
+</div>
+<div class="row border-active justify-content-between backcolorwhite MyMovies">
+    <div class="tab-content col-md-auto col-sm-auto col-auto margin-top" id="myTabContent">
+        <!-- Меню фильмов-->
+        <div class="tab-pane fade show active margin-left" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Популярные</a>
+                </li>
+
+                <li class="nav-item margin-item">
+                    <a class="nav-link" href="#">Новинки</a>
+                </li>
+                <li class="nav-item dropdown margin-item">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Жанр</a>
+                    <div class="dropdown-menu container">
+                        <div class="row dropdown-menu-row-genres">
+                            <span class="dropdown-item col-lg-3 search-genre" id="test">Биография</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Боевик</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Военный</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Детектив</span>
+                        </div>
+                        <div class="row dropdown-menu-row-genres">
+                            <span class="dropdown-item col-lg-3 search-genre">Драма</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Комедия</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Криминал</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Мелодрама</span>
+                        </div>
+                        <div class="row dropdown-menu-row-genres">
+                            <span class="dropdown-item col-lg-3 search-genre">Музыка</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Мультфильм</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Приключения</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Триллер</span>
+                        </div>
+                        <div class="row dropdown-menu-row-genres">
+                            <span class="dropdown-item col-lg-3 search-genre">Ужасы</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Фантастика</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Фэнтези</span>
+                        </div>
+                        <!--<div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a-->
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+
+        <!-- Меню сериалов-->
+        <div class="tab-pane fade margin-left" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Популярные</a>
+                </li>
+
+                <li class="nav-item margin-item">
+                    <a class="nav-link" href="#">Новинки</a>
+                </li>
+                <li class="nav-item dropdown margin-item">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Жанр</a>
+                    <div class="dropdown-menu container">
+                        <div class="row dropdown-menu-row-genres">
+                            <span class="dropdown-item col-lg-3 search-genre">Биография</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Боевик</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Военный</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Детектив</span>
+                        </div>
+                        <div class="row dropdown-menu-row-genres">
+                            <span class="dropdown-item col-lg-3 search-genre">Драма</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Комедия</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Криминал</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Мелодрама</span>
+                        </div>
+                        <div class="row dropdown-menu-row-genres">
+                            <span class="dropdown-item col-lg-3 search-genre">Музыка</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Мультфильм</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Приключения</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Триллер</span>
+                        </div>
+                        <div class="row dropdown-menu-row-genres">
+                            <span class="dropdown-item col-lg-3 search-genre">Ужасы</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Фантастика</span>
+                            <span class="dropdown-item col-lg-3 search-genre">Фэнтези</span>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class=" col-md-auto col-sm-auto col-auto margin-top paddingimp">
+        <form class="form-inline my-lg-0">
+            <input class="form-control mr-sm-2 col-md col-sm" type="search" placeholder="Поиск">
+        </form>
+        <a href="#"><p class="text-capitalize text-nowrap">Расширенный поиск</p></a>
+    </div>
+</div>
+<div class="row margin justify-content-around  MyMovies" style="padding-top: 50px;" id="colorposter">
+</div>
