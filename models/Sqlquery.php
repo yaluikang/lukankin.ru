cc99441f762d9ba016f72e7f444246bc6603fe06
yaluikang <lukankin.aleksandr@mail.ru>
2020-04-25 05:03:48 +0300

    Rebuilding action pagination 3.1

--- models/Sqlquery.php
+++ models/Sqlquery.php
@@ -15,7 +15,7 @@ class Sqlquery extends Query
         //$sql = $this->select(["movies_id, movies_name, movies_url_poster, movies_date, GROUP_CONCAT(genres_name SEPARATOR ', ') AS `genres_name` FROM (SELECT movies.movies_id, movies_name, genres_name, movies_url_poster, movies_date FROM movies JOIN movies_has_genre ON `movies`.`movies_id`=`movies_has_genre`.`movies_id` JOIN genres ON `movies_has_genre`.`movies_genre`=`genres`.`genres_id` WHERE `movies_qualifier`=1 AND movies.movies_id= ANY(SELECT movies.movies_id FROM movies JOIN movies_has_genre ON `movies`.`movies_id`=`movies_has_genre`.`movies_id` JOIN genres ON `movies_has_genre`.`movies_genre`=`genres`.`genres_id` WHERE `movies_qualifier`=1)) AS `table`"])->groupBy('movies_id')->limit(9)->all();
         //$sql = $this->createCommand();
         $borders = $this->getBorders();
-        $movies = $this->select("movies_id, movies_name, movies_url_poster, movies_date")->FROM("movies")->offset( $borders['offset'] )->limit( $borders['limit'] )->createCommand()->sql;
+        $movies = $this->select("movies_id, movies_name, movies_url_poster, movies_date")->FROM("movies")->offset( $borders['offset'] )->limit( $borders['limit'] )->all();
         echo json_encode( $movies,JSON_UNESCAPED_UNICODE );
         //$moviesWithGenres = $this->addGenresForMovies($movies);
         //return json_encode( $moviesWithGenres,JSON_UNESCAPED_UNICODE );
