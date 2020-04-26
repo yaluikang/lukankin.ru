a646423c4dc696004bb0fc594f8e1e1b2ad757b1
yaluikang <lukankin.aleksandr@mail.ru>
2020-04-26 20:20:56 +0300

    bugfix Sqlquery.php 1.8

--- models/Sqlquery.php
+++ models/Sqlquery.php
@@ -12,8 +12,6 @@ class Sqlquery extends Query
 
     public function getMovies()
     {
-        //$sql = $this->select(["movies_id, movies_name, movies_url_poster, movies_date, GROUP_CONCAT(genres_name SEPARATOR ', ') AS `genres_name` FROM (SELECT movies.movies_id, movies_name, genres_name, movies_url_poster, movies_date FROM movies JOIN movies_has_genre ON `movies`.`movies_id`=`movies_has_genre`.`movies_id` JOIN genres ON `movies_has_genre`.`movies_genre`=`genres`.`genres_id` WHERE `movies_qualifier`=1 AND movies.movies_id= ANY(SELECT movies.movies_id FROM movies JOIN movies_has_genre ON `movies`.`movies_id`=`movies_has_genre`.`movies_id` JOIN genres ON `movies_has_genre`.`movies_genre`=`genres`.`genres_id` WHERE `movies_qualifier`=1)) AS `table`"])->groupBy('movies_id')->limit(9)->all();
-        //$sql = $this->createCommand();
         $borders = $this->getBorders();
         $movies = $this->select("movies_id, movies_name, movies_url_poster, movies_date")->FROM("movies")->offset( $borders['offset'] )->limit( $borders['limit'] )->all();
         $moviesWithGenres = $this->addGenresForMovies($movies);
@@ -27,7 +25,7 @@ class Sqlquery extends Query
         $this->limit = null;
         $this->offset = null;
         $genresOfMovies = $this->select('movies_id, genres_name' )->FROM('movies_has_genre' )->join('JOIN', 'genres', 'movies_has_genre.movies_genre=genres.genres_id' )->where(['movies_id' => $ids ])->createCommand()->sql;
-        /*for( $i = 0; $i < count( $movies ); $i++ )
+        for( $i = 0; $i < count( $movies ); $i++ )
         {
             $movies[$i]['genres_name'] = [];
             for( $j = 0; $j < count( $genresOfMovies ); $j++)
@@ -35,11 +33,10 @@ class Sqlquery extends Query
                 if( $movies[$i]['movies_id'] == $genresOfMovies[$j]['movies_id'] )
                 {
                     array_push( $movies[$i]['genres_name'], $genresOfMovies[$j]['genres_name']);
-                    //$movies[$i]['genres_name'] = $genresOfMovies[$j]['genres_name'];
                 }
             }
-        }*/
-        return $genresOfMovies; //array_merge_recursive( $movies, $genresOfMovies );
+        }
+        return $movies;
     }
     public function getIdOfMovies($movies)
     {
