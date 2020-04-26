3c5926fa9706b60746ef8aa63113db9c54a8b709
yaluikang <lukankin.aleksandr@mail.ru>
2020-04-26 20:13:40 +0300

    bugfix Sqlquery.php 1.6

--- models/Sqlquery.php
+++ models/Sqlquery.php
@@ -25,7 +25,7 @@ class Sqlquery extends Query
         //цикл для инъекции жанров в массив $movies
         $ids = $this->getIdOfMovies( $movies );
         $this->limit = null;
-        $genresOfMovies = $this->select('movies_id, genres_name' )->FROM('movies_has_genre' )->join('JOIN', 'genres', 'movies_has_genre.movies_genre=genres.genres_id' )->where(['movies_id' => $ids ])->createCommand()->sql;
+        $genresOfMovies = $this->select('movies_id, genres_name' )->FROM('movies_has_genre' )->join('JOIN', 'genres', 'movies_has_genre.movies_genre=genres.genres_id' )->where(['movies_id' => $ids ])->limit(null)->createCommand()->sql;
         /*for( $i = 0; $i < count( $movies ); $i++ )
         {
             $movies[$i]['genres_name'] = [];
@@ -38,7 +38,7 @@ class Sqlquery extends Query
                 }
             }
         }*/
-        return $this->limit; //array_merge_recursive( $movies, $genresOfMovies );
+        return $genresOfMovies; //array_merge_recursive( $movies, $genresOfMovies );
     }
     public function getIdOfMovies($movies)
     {
