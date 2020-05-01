85c4d5393c5be59898836bd1231e838a97c07cfd
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-01 03:01:25 +0300

    Working version with films and tvSerias

--- web/js/index.js
+++ web/js/index.js
@@ -1,36 +1,3 @@
-/*
-
-let $ajaxBuilder = new AjaxBuilder("http://lukankin.ru/getmovies", "GET");
-
-
-$(window).on('load',function(){
-    $ajaxBuilder.ajaxRequest();
-});
-
-$('#see-more').on( 'click', function(){
-    $ajaxBuilder.setUrl( "http://lukankin.ru/pagination" );
-    $ajaxBuilder.ajaxRequest();
-});
-
-$('#films').on( 'click', function(){
-    //удалить весь контент с фильмами
-    //сделать ajax-запрос по фильмам
-    $('#colorposter').empty();
-    let $getFilms = new AjaxBuilder("http://lukankin.ru/getmovies", "GET");
-    $getFilms.setMoviesQualifier( 1 );
-    console.log( $getFilms.getMoviesQualifier());
-    $getFilms.ajaxRequest();
-});
-
-$('#tvSeries').on( 'click', function(){
-    //удалить весь контент с сериалами
-    //сделать ajax-запрос по сериалам
-    $('#colorposter').empty();
-    let $tvSeries = new AjaxBuilder("http://lukankin.ru/getmovies", "GET");
-    $tvSeries.setMoviesQualifier( 2 );
-    console.log( $tvSeries.getMoviesQualifier());
-    $tvSeries.ajaxRequest();
-});*/
 
 let $pagination = new Pagination("http://lukankin.ru/getmovies", "GET");
 
