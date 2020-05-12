46fa4642c60251ae4cbc14319dd343537bccc411
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-12 05:49:19 +0300

    checkThePage 2.2

--- views/site/index.php
+++ views/site/index.php
@@ -3,7 +3,7 @@
     if( $_SERVER['REQUEST_URI'] == '/tvseries' )
     {
         $this->registerJsFile('js/tvseries.js', ['depends' => [\app\assets\AppAsset::class]]);
-    } else if( $_SERVER['REQUEST_URI'] == '/films' || $_SERVER['REQUEST_URI'] == '/index' )
+    } else if( $_SERVER['REQUEST_URI'] == '/films' || $_SERVER['REQUEST_URI'] == '/' )
     {
         $this->registerJsFile('js/films.js', ['depends' => [\app\assets\AppAsset::class]]);
     }
