af4664d9e9c619156ad36f1e214653f59cda9fd2
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-16 01:35:30 +0300

    search 1.7

--- web/js/search.js
+++ web/js/search.js
@@ -1,10 +1,8 @@
 $(window).on('load',function(){
     $pagination.setUrl("http://lukankin.ru/searchform");
-    //$pagination.setGetParameter('search', $(this).text());
     const $queryString = window.location.search;
-    console.log( $queryString );
     const urlParams = new URLSearchParams( $queryString );
     const $search = urlParams.get( 'search' );
-    console.log( $search );
-    //$pagination.ajaxRequest();
+    $pagination.setGetParameter('search', $search );
+    $pagination.ajaxRequest();
 });
