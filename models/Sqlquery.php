238d597281ad5bf09d2171989a0583175e7c6887
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-16 01:02:23 +0300

    search 1.1

--- models/Sqlquery.php
+++ models/Sqlquery.php
@@ -25,7 +25,7 @@ class Sqlquery extends Query
         //если ничего не найдено - провести поиск по жанрам
         //отдать объект json с найденными результами
         //если ничего не нашлось - отдать ошибку в поиске
-        $movies = $this->select("movies_id, movies_name, movies_url_poster, movies_date")->FROM("movies")->where(['like', 'movies_name', $search, 'or like', 'movies_date', $search ])->createCommand()->sql;
+        $movies = $this->select("movies_id, movies_name, movies_url_poster, movies_date")->FROM("movies")->where(['like', 'movies_name', $search])->createCommand()->sql;
         echo json_encode( $movies,JSON_UNESCAPED_UNICODE );
     }
 
