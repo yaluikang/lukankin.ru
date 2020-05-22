9b1f714424958c7b219e1790787ae2295c55f124
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-22 04:29:24 +0300

    lc 1.3

--- views/site/account.php
+++ views/site/account.php
@@ -8,7 +8,7 @@ $this->registerCssFIle("@web/css/account.css");
 
  ?>
 <div class="row margin justify-content-around  MyMovies" style="padding-top: 50px;" id="colorposter">
-    <div class="account col-md-12 col-sm-12 col-lg-12 text-center row justify-content-center">
+    <div class="account col-md-12 col-sm-12 col-lg-12 text-center row justify-content-center" id="account">
         <!--<div class="col-lg-auto shipping-form" id="authorization">
             <h5 class="color-white font-weight-600">Здесь будет личный кабинет</h5>
 
