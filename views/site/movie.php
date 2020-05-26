7ead798dea4f44895be2736c15d9b12501ab0dea
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-26 04:46:41 +0300

    Bookmark 1.8

--- views/site/movie.php
+++ views/site/movie.php
@@ -27,7 +27,7 @@ $this->registerJsFile('js/movie.js', ['depends' => [\app\assets\AppAsset::class]
                         <img src="../images/bookmark.png" id="bookmark">
                     </div>
                     <div style="display: inline-block;">
-                        <img src="../images/bookmarkused.png.png" id="bookmarkused">
+                        <img src="../images/bookmarkused.png" id="bookmarkused">
                     </div>
                 </div>
                 <p class="col-md-12 col-sm-12 col-12">The Ticket</p>
