11d15c536102d42db665a04f99b522f957bc8869
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-26 05:05:00 +0300

    Bookmark 2.1

--- web/js/movie.js
+++ web/js/movie.js
@@ -1,9 +1,13 @@
 $('#bookmark').on('click', function(){
+    $(this).removeClass('display-inline');
     $(this).addClass('display-none');
+    $('#bookmarkused').removeClass('display-none');
     $('#bookmarkused').addClass('display-inline');
 });
 
 $('#bookmarkused').on('click', function(){
+    $(this).removeClass('display-inline');
     $(this).addClass('display-none');
+    $('#bookmark').removeClass('display-none');
     $('#bookmark').addClass('display-inline');
 });
