ab9e565d9928ac76eab26cce987a348e9d065bb3
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-29 01:56:50 +0300

    Cookies to DB 1.6

--- models/UserBookmarks.php
+++ models/UserBookmarks.php
@@ -5,6 +5,7 @@ namespace app\models;
 
 
 use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
 
 class UserBookmarks extends ActiveRecord
 {
@@ -17,6 +18,7 @@ class UserBookmarks extends ActiveRecord
     public static function addBookmarksToDb()
     {
         $list = static::find()->select('movie_id')->where(['user_id' => 1])->asArray()->all();
+        $list = ArrayHelper::getColumn($list,'movie_id');
         echo json_encode( $list,JSON_UNESCAPED_UNICODE );
     }
 
