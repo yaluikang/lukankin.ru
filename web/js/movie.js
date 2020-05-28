441e3ad7e19886d12a4ee92f99c74d1d68603a37
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-28 03:39:13 +0300

    Cookie 1.1

--- web/js/movie.js
+++ web/js/movie.js
@@ -2,7 +2,7 @@ $('#bookmark').on('click', function(){
     $(this).addClass('display-none');
     $('#bookmarkused').removeClass('display-none');
     Cookies.set('name', 'value', { expires: 7, path: '/' });
-    alert(Cookies.get('name'););
+    alert(Cookies.get('name'));
 });
 
 $('#bookmarkused').on('click', function(){
