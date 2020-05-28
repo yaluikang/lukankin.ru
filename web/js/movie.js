4b8b7650dd62b053536424f28de46f5158b0a19a
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-28 04:09:24 +0300

    Cookie 1.9

--- web/js/movie.js
+++ web/js/movie.js
@@ -1,5 +1,5 @@
 $('#bookmark').on('click', function(){
-    let $fimsId = $(this).attr('id');
+    let $fimsId = $(this).attr('data-movie-id');
     console.log( $fimsId );
     Cookies.set( $fimsId, true, { expires: 1, path: '/' });
     $(this).addClass('display-none');
