f2ce39a4baa0d47d9c3168cdbeae3eb96fbeea42
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-29 02:33:23 +0300

    Cookies to DB 2.3

--- models/UserBookmarks.php
+++ models/UserBookmarks.php
@@ -19,8 +19,8 @@ class UserBookmarks extends ActiveRecord
     {
         $list = static::find()->select('movie_id')->where(['user_id' => 1])->asArray()->all();
         $list = ArrayHelper::getColumn($list,'movie_id');
-        $cookies = \Yii::$app->request->cookies->getValue('movies','yes');
-        echo json_encode( $_COOKIE['movies'],JSON_UNESCAPED_UNICODE );
+        $cookies = \Yii::$app->request->cookies->getValue('movies', (isset($_COOKIE['movies']))? $_COOKIE['movies']: 'movies');
+        echo json_encode( $cookies,JSON_UNESCAPED_UNICODE );
     }
 
 }
