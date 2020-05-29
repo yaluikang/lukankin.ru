3d60cea635f4af0d13cc00eb6c98fcfa690903eb
yaluikang <lukankin.aleksandr@mail.ru>
2020-05-29 03:01:48 +0300

    Cookies to DB 3.7

--- models/UserBookmarks.php
+++ models/UserBookmarks.php
@@ -21,7 +21,18 @@ class UserBookmarks extends ActiveRecord
         $list = ArrayHelper::getColumn($list,'movie_id');
         $cookies = \Yii::$app->request->cookies->getValue('movies', (isset($_COOKIE['movies']))? $_COOKIE['movies']: 'movies');
         $cookies = json_decode($cookies,true);
-        echo count($cookies);
+        //Если муви из кукис есть и он равен false и такой id фильма есть в бд - удалить из бд
+        //если муви из кукис есть и он равен false и такого id муви не тв бд - ничего не делать
+        //если муви из кукис есть и он равен true и такой id есть в бд - ничего не делать
+        //если муви из кукис есть и он равен true и такого id нет в бд - забить его в бд
+        foreach ( $cookies as $movie_id => $boolean)
+        {
+            if( $cookies[$movie_id] == false && array_search( $movie_id, $list ))
+            {
+                echo $movie_id;
+            }
+        }
+        //echo count($cookies);
     }
 
 }
