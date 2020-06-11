644ca04da1c656d1632f566430db1fb8acd47ae6
yaluikang <lukankin.aleksandr@mail.ru>
2020-06-12 02:01:19 +0300

    Rebuild account view 1.6

--- web/js/account.js
+++ web/js/account.js
@@ -6,7 +6,7 @@ $('#content > div').each(function(){
 });*/
 $('#menu > li').each(function(){
     $(this).on('click', function(){
-        alert($(this).attr('id').slice(0,5));
-        $($(this).attr('id').slice(0,5)).addClass('active-content');
+        alert($(this).attr('id').slice(5));
+        $($(this).attr('id').slice(5)).addClass('active-content');
     });
 });
