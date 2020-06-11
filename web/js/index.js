41ba0f52515c79ca938be1c19b014b51e8b0d879
yaluikang <lukankin.aleksandr@mail.ru>
2020-06-12 02:35:23 +0300

    Rebuild account view 2.4

--- web/js/index.js
+++ web/js/index.js
@@ -47,6 +47,7 @@ $('.search-genre').each(function(){
 if($('#logout'))
 {
     $('#logout').on('click', function(){
+        alert(1);
         let $ajaxRequest = new AjaxBuilder("http://lukankin.ru/setnewcookies", "GET");
         $ajaxRequest.ajaxRequest();
     });
