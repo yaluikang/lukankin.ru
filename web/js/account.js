16e1c6673babb7be59d50b5327a8604d3fc8254c
yaluikang <lukankin.aleksandr@mail.ru>
2020-06-12 04:05:55 +0300

    Rebuild account view 4.7

--- web/js/account.js
+++ web/js/account.js
@@ -3,8 +3,8 @@ $('#menu > li').each(function(){
     $(this).on('click', function(){
         if($(this).attr('id').slice(5) == 'bookmarks')
         {
-            /*let $ajaxRequest = new AjaxBuilder("http://lukankin.ru/getbookmarks", "GET");
-            $ajaxRequest.ajaxRequest();*/
+            let $ajaxRequest = new AjaxBuilder("http://lukankin.ru/getbookmarks", "GET");
+            $ajaxRequest.ajaxRequest();
         }
         $('#content > div').each(function(){
             $(this).addClass('display-none');
