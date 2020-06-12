6cdff243640c521a268e52bc9b9b43277c2890fc
yaluikang <lukankin.aleksandr@mail.ru>
2020-06-12 03:50:29 +0300

    Rebuild account view 4.3

--- web/js/account.js
+++ web/js/account.js
@@ -6,9 +6,9 @@ $('#menu > li').each(function(){
             /*let $ajaxRequest = new AjaxBuilder("http://lukankin.ru/getbookmarks", "GET");
             $ajaxRequest.ajaxRequest();*/
         }
-        $('.active-content').each(function(){
-            $(this).removeClass('active-content');
+        $('#content > div').each(function(){
+            $(this).addClass('display-none');
         });
-        $('#' + $(this).attr('id').slice(5)).addClass('active-content');
+        $('#' + $(this).attr('id').slice(5)).removeClass('display-none');
     });
 });
