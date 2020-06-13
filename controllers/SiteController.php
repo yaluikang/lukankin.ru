5bd790baca82d728d567e3ecad6ebd317df1c641
yaluikang <lukankin.aleksandr@mail.ru>
2020-06-13 10:49:36 +0300

    upload image 1.3

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -45,8 +45,17 @@ class SiteController extends Controller
 
     public function actionAccount( $m = 'statistics')
     {
-        $markerOfMenu = $m;
-        return $this->render('account', [ 'markerOfMenu' => $markerOfMenu ]);
+        $model = new UploadForm();
+
+        if (Yii::$app->request->isPost) {
+            $model->imageFile = UploadedFile::getInstance($model, 'imageFile');
+            if ($model->upload()) {
+                // file is uploaded successfully
+                return;
+            }
+        }
+
+        return $this->render('account', ['model' => $model]);
     }
     public function actionSearch()
     {
