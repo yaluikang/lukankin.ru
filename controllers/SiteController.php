d862448c5279b544d199056bdb7008f949b9f7cb
yaluikang <lukankin.aleksandr@mail.ru>
2020-06-13 10:45:11 +0300

    upload image 1.1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -13,6 +13,8 @@ use app\models\UserRecord;
 use Yii;
 use yii\web\Controller;
 use app\models\Sqlquery;
+use app\models\UploadForm;
+use yii\web\UploadedFile;
 
 class SiteController extends Controller
 {
@@ -146,5 +148,20 @@ class SiteController extends Controller
         $contentForMovie = new Sqlquery();
         $contentForMovie = $contentForMovie->getContentForMovie( UserBookmarks::getListOfMovies(Yii::$app->user->getId()) );
         echo json_encode( $contentForMovie,JSON_UNESCAPED_UNICODE );
+    }
+
+    public function actionUpload()
+    {
+        $model = new UploadForm();
+
+        if (Yii::$app->request->isPost) {
+            $model->imageFile = UploadedFile::getInstance($model, 'imageFile');
+            if ($model->upload()) {
+                // file is uploaded successfully
+                return;
+            }
+        }
+
+        return $this->render('upload', ['model' => $model]);
     }
 }
