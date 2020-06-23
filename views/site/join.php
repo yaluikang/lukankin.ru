9966ec4d89401ed54c66317fe9c65a6fb16f4e28
yaluikang <lukankin.aleksandr@mail.ru>
2020-06-23 13:22:09 +0300

    jumbotron 1.9

--- views/site/join.php
+++ views/site/join.php
@@ -8,13 +8,14 @@ $this->registerCssFIle("@web/css/authorization.css");
     <div class="col-md-12 col-sm-12 col-lg-12 text-center row justify-content-center">
         <div class="col-lg-auto shipping-form" id="registration">
             <h5 class="color-white font-weight-600">Регистрация</h5>
-            <?php $form = ActiveForm::begin(['id' => 'user-join-form']); ?>
-            <?= $form->field($userJoinForm, 'name') ?>
-            <?= $form->field($userJoinForm, 'email') ?>
-            <?= $form->field($userJoinForm, 'password')->passwordInput(); ?>
-            <?= $form->field($userJoinForm, 'password2')->passwordInput(); ?>
+            <?php $form = ActiveForm::begin(['id' => 'user-join-form',
+                'options' => ['class' => 'color-white']]); ?>
+            <?= $form->field($userJoinForm, 'name')->label(false)->textInput(['placeholder' => 'Логин']) ?>
+            <?= $form->field($userJoinForm, 'email')->label(false)->textInput(['placeholder' => 'email']) ?>
+            <?= $form->field($userJoinForm, 'password')->label(false)->passwordInput()->textInput(['placeholder' => "Придумайте пароль"]); ?>
+            <?= $form->field($userJoinForm, 'password2')->label(false)->passwordInput()->textInput(['placeholder' => "Повторите пароль"]); ?>
             <?= \yii\helpers\Html::submitButton('Create',
-                ['class' => 'btn']) ?>
+                ['class' => '']) ?>
             <?php ActiveForm::end(); ?>
             <div class="registration_authorization row justify-content-center color-white"><div class="toggle-button" style="margin-right: 5px;">Вход </div><div>  в существующую учетную запись</div></div>
         </div>
--- views/site/login.php
+++ views/site/login.php
@@ -8,12 +8,13 @@ $this->registerCssFIle("@web/css/authorization.css");
     <div class="col-md-12 col-sm-12 col-lg-12 text-center row justify-content-center">
         <div class="col-lg-auto shipping-form" id="authorization">
             <h5 class="color-white font-weight-600">Авторизация</h5>
-            <?php $form = ActiveForm::begin(['id' => 'user-login-form']); ?>
-            <?= $form->field($userLoginForm, 'email') ?>
-            <?= $form->field($userLoginForm, 'password')->passwordInput()->label("Пароль"); ?>
-            <?= $form->field($userLoginForm, 'remember')->checkbox() ?>
+            <?php $form = ActiveForm::begin(['id' => 'user-login-form',
+                'options' => ['class' => 'color-white']]); ?>
+            <?= $form->field($userLoginForm, 'email')->textInput(['placeholder' => 'email'])->label(false) ?>
+            <?= $form->field($userLoginForm, 'password')->passwordInput()->label(false)->textInput(['placeholder' => 'Пароль']); ?>
+            <?= $form->field($userLoginForm, 'remember')->label('Запомнить')->checkbox() ?>
             <?= \yii\helpers\Html::submitButton('Enter',
-                ['class' => 'btn']) ?>
+                ['class' => '']) ?>
             <?php ActiveForm::end(); ?>
             <div class="registration_authorization row justify-content-center color-white"><div class="toggle-button" style="margin-right: 5px;"><a href="/join">Регистрация</a> </div><div>  нового пользователя</div></div>
         </div>
