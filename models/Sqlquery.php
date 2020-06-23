5045f03ff6151373b52265264c01061214fac0e3
yaluikang <lukankin.aleksandr@mail.ru>
2020-06-23 12:02:15 +0300

    Random movie 1.1

--- models/Sqlquery.php
+++ models/Sqlquery.php
@@ -22,7 +22,7 @@ class Sqlquery extends Query
     public function getCountOfMovies()
     {
         $count = $this->select("COUNT(*)")->FROM("movies")->all();
-        return $count;
+        return json_encode( $count,JSON_UNESCAPED_UNICODE );
     }
 
     public function getMovieBySearch( $search )
